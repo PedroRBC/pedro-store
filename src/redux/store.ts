83a13af7498c5d6dcd89ea1616fcfac9cc685c12
pedrorbc <pedrinhorbc@gmail.com>
2023-06-08 17:27:24 +0000

    chore: Remove redux unused files

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,11 @@
 'use client';
 
 import { configureStore } from "@reduxjs/toolkit";
-import { userReducer } from "./features/user/user-slice";
 import { productsReducer } from "./features/products/products-slice";
 
 
 export const store = configureStore({
     reducer: {
-        user: userReducer,
         products: productsReducer
     }
 });
